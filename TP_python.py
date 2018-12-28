65fb7fe1dc3496f33eea935e47b1f1b3b026e120
Thomas.Coutant <thomas.coutant@ynov.com>
2018-12-28 16:26:05 +0100

    premier commit

---
+++ TP_python.py
@@ -0,0 +1,22 @@
+import argparse 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_directory",
+                    action="store",
+                    type=str,
+                    required=True,
+                    #dest=input,
+                    help="Input directory, please make sure you have insert the input directory, containing the markdown files.",
+                    )
+parser.add_argument("-o", "--output-directory",
+                    action="store",
+                    type=str,
+                    required=True,
+                    help="Ouput directory, please make sure you have insert an ouput directory.",
+                    )
+parser.add_argument("-t", "--template-directory",
+                    action="store",
+                    type=str,
+                    help="Template directory, please make sure you have insert an template directory.",
+                    )
+arg = parser.parse_args()
