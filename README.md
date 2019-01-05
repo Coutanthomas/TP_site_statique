e97d554974b5002f0bdc4b391b88164437726c58
Thomas.Coutant <thomas.coutant@ynov.com>
2019-01-06 00:32:34 +0100

    test list

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# TP_site_statique
+# TP_site_statique.
 Le dossier selectionné en sortie (output_directory), ne doit pas être un dossier déjà existant. Lors du choix de dossier "output_directory" il faudra donc choisir un dossier inexistant dans votre repertoire. Ce dossier sera créer par le programme, afin d'eviter tout écrassement de fichier non voulu.
 
+## Argument:
+-i : Sélection du dossier "d'entrée". Le dossier qui contient vos fichier markdown que vous souhaité convertir en html.
+-o : Sélection du dossier de "sortie". Le dossier qui contiendra:   *Soit seulement les fichier markdown convertie en html.
+                                                                    *Soit un seul fichier html contenant tout les fichier d'entrée convertie avec le template. 
+-t : Sélection du dossier contenant le model de la page internet. 
+-n : Vous permet de choisir le nom de votre nouveaux fichier html. Qui contiendra tout les markdown convertit, associé au template choisis.
--- TP_python.py
+++ TP_python.py
@@ -22,7 +22,13 @@ parser.add_argument("-o", "--output-directory",
 parser.add_argument("-t", "--template-directory",
                     action="store",
                     type=str,
+                    default=None,
                     help="Template directory, containing web pages template.",
+                    )
+parser.add_argument("-n", "--new-name-template",
+                    action="store",
+                    type=str,
+                    help="Name for your new template, with all files translate.",
                     )
 args = parser.parse_args()
 
@@ -41,14 +47,16 @@ if not os.path.exists(args.output_directory):
     #print(args.output_directory + " : The path you chose does not exist. Please make sure you have insert a write output directory")
 
 
-if not os.path.exists(args.template_directory):
-    print("there are no template selected, you only have a conversion markdown to html")
-else:
-    print("Generate with template " + os.path.basename(args.template_directory))   
+#if not os.path.exists(args.template_directory):
+#    print("there are no template selected, you only have a conversion markdown to html")
+#else:
+#    print("Generate with template " + os.path.basename(args.template_directory))   
 
-
+list_att = []
 
 def marktohtml(line):
+    cont_nb_element_list = "0"
+    compteur_list = "1"
     html_line = ""
     if line[:3] == "###":
         html_line = "<h3>" + line[3:].rstrip() + "</h3>"
@@ -56,29 +64,32 @@ def marktohtml(line):
         html_line = "<h2>" + line[2:].rstrip() + "</h2>"
     elif line[:1] == "#":
         html_line = "<h1>" + line[1:].rstrip() + "</h1>"
-    elif line[:1] == "*":
-        if line.count("*")%2 == 1:
-            html_line = "<il>" + line[1:] + "</il>"
-    elif line.count("*") > 1:
-        html_line = "<em>".join(line.split("*"))
-        html_line = html_line.split(" ")
-        deb = ""
-        tour = []
-        cont = []
-        new = ""
-        new1 = []
-        for i in html_line:
-            if i.count("<em>") == 2:
-                deb = deb + "0"
-                new = i[:-4] + "</em>"
-                new1.append(new)
-                cont.append(len(tour))
-            tour.append(1)
-        k = deb.count("0")
-        while k > 0:
-            html_line[cont[k-1]] = new1[k-1]
-            k -= 1
-        html_line = " ".join(html_line)         
+    elif line.count("*") >= 1:
+        if line.count("*")%2 == 0:
+            html_line = "<em>".join(line.split("*"))
+            html_line = html_line.split(" ")
+            deb = ""
+            tour = []
+            cont = []
+            new = ""
+            new1 = []
+            for i in html_line:
+                if i[-4:].count("<em>"):
+                    deb = deb + "0"
+                    new = i[:-4] + "</em>"
+                    new1.append(new)
+                    cont.append(len(tour))
+                tour.append(1)
+            k = deb.count("0")
+            while k > 0:
+                html_line[cont[k-1]] = new1[k-1]
+                k -= 1
+            html_line = " ".join(html_line)
+            print("non")
+        else:  
+            cont_nb_element_list += 1
+            list_att.append(line)
+            "".join(list_att)     
     elif line.count("http://") >= 1:
         html_line = line.split(" ")
         tour = []
@@ -100,6 +111,8 @@ def marktohtml(line):
         html_line = " ".join(html_line)
     else:
         html_line = line
+    compteur_list = compteur_list + cont_nb_element_list
+    print(compteur_list) 
     return html_line
     
 
@@ -124,25 +137,46 @@ def savnew():
         with open(args.output_directory + newname1(listmd[i]), "w") as fichier:
             fichier.write(convert(listmd[i]))
         i += 1
-    print ("done")
 
-def insert_in_template():
+def insert_in_template(name_temp):
+    cont = len(listmd)
     i = 0
-    att = ""
-    with open(args.template_directory, "r") as fichier:
-        for line in fichier:
-            if line.count("<REPLACE_ME>"):
-                with open(args.output_directory + newname1(listmd[0]), "r") as fichier_o:
-                    for line1 in fichier_o:
-                        att = att + line1
-                i = 1   
-            att = att + line
-    with open(args.output_directory + newname1(listmd[0]), "w") as fichier_S:
-        fichier_S.writelines(att)
+    save = ""
+    with open(args.template_directory, "r") as fichier_t:
+        for line_t in fichier_t:
+            if line_t.count("<REPLACE_ME>"):
+                while cont > 0:    
+                    with open(args.output_directory + newname1(listmd[cont-1]), "r") as fichier_o:
+                        for line_o in fichier_o:
+                            save = save + line_o
+                        os.remove(args.output_directory + newname1(listmd[cont-1]))
+                    cont -= 1
+                    i = 1   
+            save = save + line_t
+    with open(args.output_directory + name_temp + ".html", "w") as fichier_S:
+        fichier_S.writelines(save)
     if i != 1:
         print("We don't find <REPLACE_ME>")
-       
+
 listmd = glob.glob(args.input_directory + "/*.md")
-savnew()
-insert_in_template()
+
+if args.template_directory is None:
+    print("You don't have chose any template did you just want to translate markdown in html ? (Y/N)")
+    if input() == "y":
+        print("markdown translate in html on " + args.output_directory)
+        savnew()
+    else:
+        print("exit")
+        sys.exit()
+
+if args.template_directory:
+    if not args.new_name_template:
+        savnew()
+        insert_in_template("your_new_template")
+        print("Done, with default name: \"your_new_template\"")
+    else:
+        name_temp = args.new_name_template
+        savnew()
+        insert_in_template(name_temp)
+        print("Done")
 
