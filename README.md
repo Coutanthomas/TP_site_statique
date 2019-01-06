8a77d9b381c58ab9abd8f864cc1d9057e8dea289
Thomas.Coutant <thomas.coutant@ynov.com>
2019-01-06 18:54:20 +0100

    explication programme

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# TP_site_statique.
-Le dossier selectionné en sortie (output_directory), ne doit pas être un dossier déjà existant. Lors du choix de dossier "output_directory" il faudra donc choisir un dossier inexistant dans votre repertoire. Ce dossier sera créer par le programme, afin d'eviter tout écrassement de fichier non voulu.
+# TP site statique
+Ce TP est un projet de classe qui nous a été proposé par notre professeur Mr.Poulailleau Vincent. Il à pour but de convertir des fichiers markdown ".md" en ".html". Pour pouvoir générer des sites statiques.
+
+Sujet: https://github.com/vpoulailleau/site_statique
+
+## Explication
+
+Il faudra donc entrer obligatoirement un dossier d'entrée contenant les fichier markdown que vous voulez convertir. Ce dossier peut également contenir d'autre fichier, toute fois ils ne seront pas pris en compte par le programme. Le programme ne convertira que les fichier markdown.
+Il faut aussi entrer un dossier de sorti qui n'existe pas déjà le programme vous le créera. Dans ce nouveau dossier vous trouverez soit vos fichier markdown converti en fichier html(cas ou il n'y a pas de template sélectionné). Soit le contenu du dossier template ainsi que la conversion des fichier markdown en html dans un seul fichier nommé "your_new_template.html" si l'option n n'a pas été utilisé.
+ 
+Deux option sont possible :
+
+ * -t Le choix du modèle de page internet. Qu'il vous faudra vous procurer sur internet, un exemple vous est donné dans se rappport (voir plus haut).Il faudra également rajouter `<REPLACE_ME>` dans le fichier index.html qui se trouve à la racine de votre dossier template. Vos fichier markdown converti se trouveront à cette place. 
+ * -n Le nom de votre nouveau fichier html associé au template choisi.
 
 ## Argument:
--i : Sélection du dossier "d'entrée". Le dossier qui contient vos fichier markdown que vous souhaité convertir en html.
--o : Sélection du dossier de "sortie". Le dossier qui contiendra:   *Soit seulement les fichier markdown convertie en html.
-                                                                    *Soit un seul fichier html contenant tout les fichier d'entrée convertie avec le template. 
--t : Sélection du dossier contenant le model de la page internet. 
--n : Vous permet de choisir le nom de votre nouveaux fichier html. Qui contiendra tout les markdown convertit, associé au template choisis.
+ * -i : Sélection du dossier "d'entrée". Le dossier qui contient vos fichier markdown que vous souhaité convertir en html.
+ * -o : Sélection du dossier de "sortie". Le dossier qui contiendra: 
+ * * Soit seulement les fichier markdown convertie en html.
+ * * Soit un seul fichier html contenant tout les fichier d'entrée convertie avec le template. 
+ * -t : Sélection du fichier contenant le model de la page internet dans votre dossier template. En générale se fichier ce nomme : index.html.
+ * -n : Vous permet de choisir le nom de votre nouveaux fichier html.
+ * -h : Commande d'aide.
--- TP_python.py
+++ TP_python.py
@@ -4,6 +4,10 @@ import os
 import glob
 import math
 import sys
+import markdown2
+from markdown2 import Markdown
+import shutil
+from shutil import ignore_patterns
 
 
 parser = argparse.ArgumentParser()
@@ -41,86 +45,35 @@ if not os.path.exists(args.input_directory):
 if not os.path.exists(args.output_directory):
     print("creating output_directory " + args.output_directory)
     os.makedirs(args.output_directory)
-#else:
-    #print(args.output_directory + " The output directory already exist, please enter a new directory name.")
-    #sys.exit()
-    #print(args.output_directory + " : The path you chose does not exist. Please make sure you have insert a write output directory")
-
-
-#if not os.path.exists(args.template_directory):
-#    print("there are no template selected, you only have a conversion markdown to html")
-#else:
-#    print("Generate with template " + os.path.basename(args.template_directory))   
-
-list_att = []
-
-def marktohtml(line):
-    cont_nb_element_list = "0"
-    compteur_list = "1"
-    html_line = ""
-    if line[:3] == "###":
-        html_line = "<h3>" + line[3:].rstrip() + "</h3>"
-    elif line[:2] == '##':
-        html_line = "<h2>" + line[2:].rstrip() + "</h2>"
-    elif line[:1] == "#":
-        html_line = "<h1>" + line[1:].rstrip() + "</h1>"
-    elif line.count("*") >= 1:
-        if line.count("*")%2 == 0:
-            html_line = "<em>".join(line.split("*"))
-            html_line = html_line.split(" ")
-            deb = ""
-            tour = []
-            cont = []
-            new = ""
-            new1 = []
-            for i in html_line:
-                if i[-4:].count("<em>"):
-                    deb = deb + "0"
-                    new = i[:-4] + "</em>"
-                    new1.append(new)
-                    cont.append(len(tour))
-                tour.append(1)
-            k = deb.count("0")
-            while k > 0:
-                html_line[cont[k-1]] = new1[k-1]
-                k -= 1
-            html_line = " ".join(html_line)
-            print("non")
-        else:  
-            cont_nb_element_list += 1
-            list_att.append(line)
-            "".join(list_att)     
-    elif line.count("http://") >= 1:
-        html_line = line.split(" ")
-        tour = []
-        cont = []
-        toto = ""
-        new = ""
-        new1 = []
-        for i in html_line:
-            if i.count("http://"):
-                toto = toto + "0"
-                new ="<a href=\"" + i + "\">" + i +"</a>"
-                new1.append(new)
-                cont.append(len(tour))
-            tour.append(1)
-        k = toto.count("0")
-        while k > 0:
-            html_line[cont[k-1]] = new1[k-1]
-            k -= 1
-        html_line = " ".join(html_line)
-    else:
-        html_line = line
-    compteur_list = compteur_list + cont_nb_element_list
-    print(compteur_list) 
-    return html_line
-    
-
+   
 def convert(path):
+    markdowner = Markdown()
     convhtml = ""
     with open(path,'r') as f:
         for line in f:
-           convhtml = convhtml + marktohtml(line) 
+            http = markdowner.convert(line)
+            if http.count("://"):
+                if not http.count("]http"):
+                    html_line = http.split(" ")
+                    tour = []
+                    cont = []
+                    toto = ""
+                    new = ""
+                    new1 = []
+                    for i in html_line:
+                        if i.count("http"):
+                            toto = toto + "0"
+                            new ="<a href=\"" + i + "\">" + i +"</a>"
+                            new1.append(new)
+                            cont.append(len(tour))
+                        tour.append(1)
+                    k = toto.count("0")
+                    while k > 0:
+                        html_line[cont[k-1]] = new1[k-1]
+                        k -= 1
+                    http = " ".join(html_line)
+            convhtml = convhtml + http
+           #convhtml = convhtml + marktohtml(line)
     return convhtml
 
 
@@ -155,6 +108,11 @@ def insert_in_template(name_temp):
             save = save + line_t
     with open(args.output_directory + name_temp + ".html", "w") as fichier_S:
         fichier_S.writelines(save)
+    shutil.copy(args.output_directory + name_temp + ".html", args.input_directory)
+    shutil.rmtree(args.output_directory)
+    shutil.copytree(os.path.dirname(args.template_directory), args.output_directory, ignore=ignore_patterns(os.path.basename(args.template_directory)))
+    shutil.copy(args.input_directory + name_temp + ".html", args.output_directory)
+    os.remove(args.input_directory + name_temp + ".html")
     if i != 1:
         print("We don't find <REPLACE_ME>")
 
@@ -168,7 +126,6 @@ if args.template_directory is None:
     else:
         print("exit")
         sys.exit()
-
 if args.template_directory:
     if not args.new_name_template:
         savnew()
